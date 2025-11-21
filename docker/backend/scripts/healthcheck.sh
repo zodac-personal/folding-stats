aac21f5c0af8b5c44034a6a537300730d68f9ec0
zodac <arouge110@msn.com>
2025-11-21 15:20:37 +1300

    Updating versions

--- docker/backend/scripts/healthcheck.sh
+++ docker/backend/scripts/healthcheck.sh
@@ -1,6 +1,7 @@
 #!/busybox sh
 
-# Since production uses HTTPS but dev does not, we try both URLs
-/busybox wget --no-check-certificate --quiet --tries=1 --spider https://127.0.0.1:8443/folding/actuator/health || \
-/busybox wget --no-check-certificate --quiet --tries=1 --spider http://127.0.0.1:8443/folding/actuator/health || \
-exit 1
+if [ "${DEPLOYMENT_TYPE}" = "production" ]; then
+  /busybox wget --no-check-certificate --quiet --tries=1 --spider https://127.0.0.1:8443/folding/actuator/health || exit 1
+else
+  /busybox wget --quiet --tries=1 --spider http://127.0.0.1:8443/folding/actuator/health || exit 1
+fi
